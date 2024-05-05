953c3fe7f82a096d051311a2c5b01a036e3f8587
Chaewon <cw06155@gmail.com>
2024-05-05 19:30:49 +0900

    my page ui

--- REACT + TS/src/components/account/MemberInfo.tsx
+++ REACT + TS/src/components/account/MemberInfo.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { useNavigate } from 'react-router-dom';
-import { Separator } from '@radix-ui/react-separator';
+import { Separator } from '../ui/separator';
 
 import { useSetRecoilState } from 'recoil';
 import { isLoginState } from '@/types/Recoil';
@@ -17,7 +17,7 @@ export default function MemberInfo() {
   };
 
   const handleWithdrawal = () => {
-    if (confirm('Do you want to proceed with membership withdrawal? ')) {
+    if (confirm('Do you want to proceed with membership withdrawal?')) {
       alert('Exited.');
       localStorage.removeItem('account');
       localStorage.removeItem('isLogin');
@@ -33,13 +33,44 @@ export default function MemberInfo() {
   }
 
   return (
-    <div>
-      <div className="flex flex-col justify-center gap-10 items-center py-20">
-        <h1 className="text-xl">My Page</h1>
+    <div className="flex justify-center">
+      <div
+        className="flex flex-col gap-5 p-20 w-full"
+        style={{ maxWidth: '1700px' }}
+      >
+        <h1 className="text-xl font-regular">MY PAGE</h1>
+        <p>{`Welcome ${id}`}</p>
         <Separator />
-        <h1>{`Welcome ${id}`}</h1>
-        <Button onClick={handleLogout}>LOGIN OUT</Button>
-        <Button onClick={handleWithdrawal}>WITHDRAWAL</Button>
+        <div className="flex justify-between bg-gray-200/50 p-5">
+          <div className="w-1/2">
+            <div className="pb-3">Balance</div>
+            <div>Order History</div>
+          </div>
+          <div className="w-1/2">
+            <div className="pb-3">Used</div>
+            <div>Coupons</div>
+          </div>
+        </div>
+        <div className="pb-10">
+          <div className="bg-gray-200/50 p-3">ORDER STATUS (Last 3 months)</div>
+          <div className="flex flex-wrap justify-between text-center items-center p-5">
+            <span className="w-full sm:w-1/5">Awaiting Payment</span>
+            <span className="w-full sm:w-1/5">Preparing Shipment</span>
+            <span className="w-full sm:w-1/5">In Transit</span>
+            <span className="w-full sm:w-1/5">Delivered</span>
+            <ul className="w-full sm:w-1/5 text-center sm:text-left">
+              <li>Cancellations: 0</li>
+              <li>Exchanges: 0</li>
+              <li>Returns: 0</li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex justify-center sm:justify-end gap-2">
+          <Button onClick={handleLogout}>LOGOUT</Button>
+          <Button onClick={handleWithdrawal} variant="link">
+            WITHDRAWAL
+          </Button>
+        </div>
       </div>
     </div>
   );
